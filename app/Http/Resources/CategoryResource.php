7b0c8267414c0b484f965278556cab07efbb9669
aswarkasim <aswarkasim@gmail.com>
2022-04-22 13:08:37 +0700

    22/04/2022 - API Category Path

--- app/Http/Resources/CategoryResource.php
+++ app/Http/Resources/CategoryResource.php
@@ -20,7 +20,9 @@ class CategoryResource extends JsonResource
             'name'     => $this->name,
             'category_type' => $this->category_type,
             'status' =>  $this->status,
-            'image' => $this->media ? env('APP_IMAGE_URL').$this->media->file_name : null,
+            'path' => $this->media ? $this->media->path : null,
+            // 'image' => $this->media ? env('APP_IMAGE_URL') . $this->media->file_name : null,
+            'image' => $this->media ? $this->media->file_name : null,
             'created_at' => $this->created_at ? $this->created_at->diffForHumans() : null,
             'updated_at' => $this->updated_at ? $this->updated_at->diffForHumans() : null,
         ];
